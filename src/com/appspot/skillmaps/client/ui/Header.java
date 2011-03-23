3040b77cf4d1bf1ed776f427e446592d1e9eb0e5
soundTricker <keisuke.oohashi@gmail.com>
2011-03-24 01:22:39 +0900

    add New Header

--- src/com/appspot/skillmaps/client/ui/Header.java
+++ src/com/appspot/skillmaps/client/ui/Header.java
@@ -3,16 +3,26 @@ package com.appspot.skillmaps.client.ui;
 
 import com.appspot.skillmaps.shared.model.Login;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Header extends Composite {
 
     private static HeaderUiBinder uiBinder = GWT.create(HeaderUiBinder.class);
+
+    public interface Style extends CssResource {
+        @ClassName("sign-in")
+        String signIn();
+    }
+
+    @UiField
+    Style style;
 
     interface HeaderUiBinder extends UiBinder<Widget, Header> {
     }
@@ -31,6 +41,7 @@ public class Header extends Composite {
             nickname.setText(login.getNickname());
             signin.setEnabled(false);
             signout.setHref(login.getLogoutUrl());
+            signPanel.addStyleName(style.signIn());
         } else {
             nickname.setText("");
             signin.setHref(login.getLoginUrl());
@@ -46,5 +57,8 @@ public class Header extends Composite {
 
     @UiField
     Anchor signout;
+
+    @UiField
+    Panel signPanel;
 
 }
