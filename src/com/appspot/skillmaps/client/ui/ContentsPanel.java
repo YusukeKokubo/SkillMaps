937126ff2263c9434747ffea95dffbadbe043d93
soundTricker <keisuke.oohashi@gmail.com>
2011-03-26 02:47:52 +0900

    add コミット忘れ

---
+++ src/com/appspot/skillmaps/client/ui/ContentsPanel.java
@@ -0,0 +1,39 @@
+package com.appspot.skillmaps.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+public class ContentsPanel extends Composite {
+
+    private static ContentsPanelUiBinder uiBinder =
+        GWT.create(ContentsPanelUiBinder.class);
+
+    @UiField(provided=true)
+    @Inject
+    @Named("contents")
+    SimplePanel mainPanel;
+
+    @UiField(provided=true)
+    @Inject
+    @Named("dashboard")
+    SimplePanel dashboard;
+
+    interface ContentsPanelUiBinder extends UiBinder<Widget, ContentsPanel> {
+    }
+
+    @Inject
+    public ContentsPanel() {
+    }
+
+    public void init(){
+        initWidget(uiBinder.createAndBindUi(this));
+        dashboard.getElement().setId("dashboard");
+    }
+
+}
