b4c4020b44b0fb50b3664106abefbf557e8ff676
yusuke.kokubo <yusuke.kokubo@gmail.com>
2011-05-18 17:35:36 +0900

    既存データがあったり、例外が起きた場合にmigrationできなかったのを修正 refs #733

--- src/com/appspot/skillmaps/server/controller/sys/CreateFollowingController.java
+++ src/com/appspot/skillmaps/server/controller/sys/CreateFollowingController.java
@@ -6,6 +6,7 @@ import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.datastore.Datastore;
 
+import com.appspot.skillmaps.server.meta.FollowingMeta;
 import com.appspot.skillmaps.server.meta.SkillRelationMeta;
 import com.appspot.skillmaps.shared.model.Following;
 import com.appspot.skillmaps.shared.model.SkillRelation;
@@ -15,17 +16,27 @@ public class CreateFollowingController extends Controller {
     @Override
     public Navigation run() throws Exception {
         SkillRelationMeta m = SkillRelationMeta.get();
-        
-        List<SkillRelation> rels = Datastore.get(m);
+        FollowingMeta fm = FollowingMeta.get();
+        List<SkillRelation> rels = Datastore.query(m).asList();
         for (SkillRelation rel : rels) {
-            String from = rel.getProfile().getUserEmail();
-            String to = rel.getSkill().getModel().getOwnerEmail();
-            
-            Following f = new Following();
-            f.setToEmail(to);
-            f.setFromEmail(from);
-            
-            Datastore.put(f);
+            try {
+                String from = rel.getUserEmail();
+                String to = rel.getSkill().getModel().getOwnerEmail();
+                
+                if (Datastore.query(fm)
+                        .filter(fm.toEmail.equal(to))
+                        .filter(fm.fromEmail.equal(from)).count() > 0) {
+                    continue;
+                }
+                
+                Following f = new Following();
+                f.setToEmail(to);
+                f.setFromEmail(from);
+                
+                Datastore.put(f);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
         return null;
     }
